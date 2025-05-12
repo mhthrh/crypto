2dd5b50b7dd6b07943401d9f90af8eb6f559767d
Mohsen Taheri <50137141+mhthrh@users.noreply.github.com>
2025-05-12 16:59:33 -0700

    add common_pkg, package manager

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"fmt"
+)
+
+type action struct {
+	msg   string
+	value string
+}
+
+var (
+	messages []action
+	counter  = 0
+)
+
+func init() {
+	messages = []action{
+		{
+			msg:   "key",
+			value: "",
+		}, {
+			msg:   "operation",
+			value: "",
+		}, {
+			msg:   "source",
+			value: "",
+		}, {
+			msg:   "destination",
+			value: "",
+		},
+	}
+}
 func main() {
-	
+	fmt.Println("crypto!!!!,")
+	for counter <= len(messages) {
+		msg := ""
+		fmt.Println(messages[counter].msg)
+		_, err := fmt.Scan(&msg)
+		if err != nil || msg == "" {
+			continue
+		}
+		messages[counter].value = msg
+		counter++
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module crypto
+
+go 1.24.2
+
+require github.com/mhthrh/common_pkg v0.0.0-20250512233835-261eab8af384 // indirect
